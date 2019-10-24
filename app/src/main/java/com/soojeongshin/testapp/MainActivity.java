e98d6562e620b1850e99e5e6ddd3d284f3f0c31d
Soojeong <soojeongshin2@gmail.com>
2019-10-25 02:51:13 +0900

    feat: Insert a new page after the current page

--- app/src/main/java/com/soojeongshin/testapp/MainActivity.java
+++ app/src/main/java/com/soojeongshin/testapp/MainActivity.java
@@ -1,19 +1,49 @@
 package com.soojeongshin.testapp;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.soojeongshin.testapp.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MyPagerAdapter mAdapter;
+    private ActivityMainBinding mBinding;
+    private List<Fragment> mFragments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        ViewPager viewPager = findViewById(R.id.viewpager);
-        MyPagerAdapter adapter = new MyPagerAdapter(MainActivity.this, getSupportFragmentManager());
-        viewPager.setAdapter(adapter);
+        mFragments = new ArrayList<>();
+        mFragments.add(SectionFragment.newInstance(0));
+        mFragments.add(SectionFragment.newInstance(1));
+        mFragments.add(SectionFragment.newInstance(2));
+
+        FragmentManager fm = getSupportFragmentManager();
+        mAdapter = new MyPagerAdapter(this, fm, mFragments);
+        mBinding.viewpager.setAdapter(mAdapter);
+
+        insert();
+    }
+
+    private void insert() {
+        mBinding.insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int currentIndex = mBinding.viewpager.getCurrentItem();
+                mAdapter.addFragmentAtPosition(currentIndex, SectionFragment.newInstance(currentIndex));
+                mBinding.viewpager.setCurrentItem(currentIndex +1);
+            }
+        });
     }
 }
--- app/src/main/java/com/soojeongshin/testapp/MyPagerAdapter.java
+++ app/src/main/java/com/soojeongshin/testapp/MyPagerAdapter.java
@@ -7,23 +7,59 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
+import java.util.List;
+
 public class MyPagerAdapter extends FragmentPagerAdapter {
 
     private final Context mContext;
+    private List<Fragment> mFragments;
 
-    public MyPagerAdapter(Context context, FragmentManager fm) {
+    public MyPagerAdapter(Context context, FragmentManager fm, List<Fragment> fragments) {
         super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
         mContext = context;
+        mFragments = fragments;
     }
 
+    void addFragmentAtPosition(int position, Fragment f) {
+        if (position == mFragments.size()) {
+            mFragments.add(f);
+        } else {
+            mFragments.add(position + 1, f);
+        }
+        notifyDataSetChanged();
+    }
+
+    /**
+     * Called when the fragments are reordered to get the changes.
+     */
+    @Override
+    public int getItemPosition(@NonNull Object object) {
+        int idx = mFragments.indexOf(object);
+        return idx < 0 ? POSITION_NONE : idx;
+    }
+
+    /**
+     * Map to a position independent ID, because the pager adapter reorders fragments.
+     */
+    @Override
+    public long getItemId(int position) {
+        return System.identityHashCode(mFragments.get(position));
+    }
+
+    /**
+     * Returns the {@link Fragment} that should be displayed for the given page number.
+     */
     @NonNull
     @Override
     public Fragment getItem(int position) {
-        return SectionFragment.newInstance(position + 1);
+        return SectionFragment.newInstance(position);
     }
 
+    /**
+     * Returns the number of views available.
+     */
     @Override
     public int getCount() {
-        return 3;
+        return mFragments.size();
     }
 }
